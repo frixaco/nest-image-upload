c4c52b5dbfb1c87b3d50756af55e977c7ea07672
frixaco <rustam21ashurmatov@gmail.com>
2022-09-15 23:15:04 +0500

    Improve content-type check

--- src/content-type.guard.ts
+++ src/content-type.guard.ts
@@ -6,6 +6,7 @@ import {
   Injectable,
 } from '@nestjs/common';
 import { Request } from 'express';
+import * as mimeTypes from 'mime-types';
 import { Observable } from 'rxjs';
 
 @Injectable()
@@ -16,9 +17,16 @@ export class ContentTypeGuard implements CanActivate {
     const request = context.switchToHttp().getRequest<Request>();
     const contentType = request.headers['content-type'];
 
-    if (!contentType || !contentType.startsWith('image/')) {
+    const mimeType = mimeTypes.lookup(request.path).toString();
+
+    if (
+      !contentType ||
+      !mimeType ||
+      !mimeType.startsWith('image/') ||
+      contentType !== mimeType
+    ) {
       throw new HttpException(
-        'Invalid Content-Type header. Should be image/*',
+        'Invalid image type',
         HttpStatus.UNSUPPORTED_MEDIA_TYPE,
       );
     }
